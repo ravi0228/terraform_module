41f65cf4b405fce4906a3d5f04d20f97b51ea2b1
ravi0228 <113755342+ravi0228@users.noreply.github.com>
2023-04-27 22:42:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
-provider "aws" {
-   region     = var.my_region
+module "web" {
+  source               = "github.com/ravi0228/terraform_module"
+  aws_region           = var.aws_region
+  aws_private_key_path = var.aws_private_key_path
+  key_name             = var.key_name
+  web_want             = var.web_want
+  web_count            = var.web_count
 }
 
-module "webserver" {
-  source = ".//module_web"  
-}
-
-module "mailserver" {
-  source = ".//module_mail"
+module "mail" {
+  source               = "github.com/ravi0228/terraform_module"
+  aws_region           = var.aws_region
+  aws_private_key_path = var.aws_private_key_path
+  key_name             = var.key_name
+  mail_want            = var.mail_want
+  mail_count           = var.mail_count
 }
